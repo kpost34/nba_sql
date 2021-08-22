23fb38dff9e0fcd8f61fb53e183d3ec20f87a830
Keith Post <kpost342005@hotmail.com>
2021-08-22 14:31:32 -0600

    Added our first SQL files

---
+++ 01_nba_queries.sql
@@ -0,0 +1,269 @@
+--some basic queries
+SELECT * FROM Draft;
+--selects all fields and records for table Draft
+
+SELECT yearDraft year, numberRound round, numberRoundPick pick 
+    FROM Draft
+    WHERE yearDraft > 2010;
+--returns draft year, round, and pick for all records from 2011 on
+    
+SELECT Count(*)
+    FROM Draft
+    WHERE yearDraft > 2010; 
+--returns count (600) of all records of players drafted since 2011
+
+SELECT nameOrganizationFrom university, numberRound round, Count(*) draft_picks
+    FROM Draft
+    WHERE yearDraft > 2010 AND University IN ('Duke', 'North Carolina')
+    GROUP BY university, round;
+--returns total number of players drafted by UNC and Duke in each round since 2011
+ 
+SELECT nameOrganizationFrom organization, yearDraft year
+    FROM Draft
+    WHERE year > 2000
+    ORDER BY year ASC, organization;
+--returns university/club and draft year from 2001 on, sorted by year then club; notice that 1191 rows returned
+    
+SELECT DISTINCT nameOrganizationFrom organization, yearDraft year
+    FROM Draft
+    WHERE year > 2000
+    ORDER BY year ASC, organization;
+--now the total records dropped to 929 (because unique universities/clubs selected)
+
+SELECT DISTINCT nameOrganizationFrom organization, yearDraft year
+    FROM Draft
+    WHERE year > 2000
+    ORDER BY year ASC, organization
+    LIMIT 50; 
+--reduces results to 50 records
+
+SELECT DISTINCT nameOrganizationFrom organization, yearDraft year
+    FROM Draft
+    WHERE year > 2000
+    ORDER BY year ASC, organization
+    LIMIT 50 OFFSET 50; 
+--also 50 records, but starting at row 51
+
+SELECT nameOrganizationFrom university, yearDraft year, Count(*) draft_picks
+    FROM Draft
+    WHERE University IN 
+        ('Georgetown', 'Syracuse', 'St. Johns', 'Villanova', 'Providence', 'Connecticut')
+        AND yearDraft > 2000
+    GROUP BY year, university
+    ORDER BY year ASC; 
+ --returns number of players drafted from original Big East schools each year (draft)
+ --since 2000 and organzied by year and university
+
+SELECT nameOrganizationFrom university, Count(*) players_drafted
+    FROM Draft
+    WHERE University IN 
+        ('Georgetown', 'Syracuse', 'St. Johns', 'Villanova', 'Providence', 'Connecticut')
+        AND yearDraft > 2000
+    GROUP BY university
+    HAVING players_drafted >= 12;
+--now this query limits the count following the grouping to at least 12 players drafted
+
+
+
+CREATE VIEW newer_games AS
+SELECT GAME_ID, SEASON_ID, GAME_DATE, ATTENDANCE, GAME_TIME, 
+    TEAM_ID_HOME, TEAM_ABBREVIATION_HOME, TEAM_NAME_HOME, TEAM_CITY_HOME, WL_HOME, PTS_HOME, 
+    MIN_HOME, FGM_HOME, FG_PCT_HOME, FG3M_HOME, FG3A_HOME, FG3_PCT_HOME, FTM_HOME, FTA_HOME, 
+    FT_PCT_HOME, OREB_HOME, DREB_HOME, REB_HOME, AST_HOME, STL_HOME, BLK_HOME, TOV_HOME, 
+    PF_HOME, PLUS_MINUS_HOME, PTS_PAINT_HOME, PTS_2ND_CHANCE_HOME, PTS_FB_HOME, 
+    LARGEST_LEAD_HOME, PTS_OFF_TO_HOME, TEAM_WINS_LOSSES_HOME,
+    TEAM_ID_AWAY, TEAM_ABBREVIATION_AWAY, TEAM_NAME_AWAY, TEAM_CITY_AWAY, WL_AWAY, PTS_AWAY, 
+    MIN_AWAY, FGM_AWAY, FG_PCT_AWAY, FG3M_AWAY, FG3A_AWAY, FG3_PCT_AWAY, FTM_AWAY, FTA_AWAY, 
+    FT_PCT_AWAY, OREB_AWAY, DREB_AWAY, REB_AWAY, AST_AWAY, STL_AWAY, BLK_AWAY, TOV_AWAY, 
+    PF_AWAY, PLUS_MINUS_AWAY, PTS_PAINT_AWAY, PTS_2ND_CHANCE_AWAY, PTS_FB_AWAY,
+    LARGEST_LEAD_AWAY, PTS_OFF_TO_AWAY, TEAM_WINS_LOSSES_AWAY
+    FROM Game
+    WHERE GAME_DATE > '2015-10-1'; 
+--creates View of games (called newer_games) from 2015-16 on and isolating select variables
+
+
+--returns view
+SELECT * FROM newer_games; --7069 rows
+
+
+--Question 1
+--How many points did the nuggets score in home and away games (in total) during the 2018-2019 season?
+SELECT GAME_DATE, TEAM_ABBREVIATION_HOME home_team, TEAM_ABBREVIATION_AWAY away_team, PTS_HOME, PTS_AWAY
+    FROM newer_games
+    WHERE SEASON_ID = 22018 AND 
+        (TEAM_ABBREVIATION_HOME = 'DEN' OR TEAM_ABBREVIATION_AWAY = 'DEN');
+--returns points for home and away teams in each of denver's games during 18-19 season
+
+SELECT SUM(PTS_HOME) total_home_points , SUM(PTS_AWAY) total_away_points
+    FROM newer_games
+    WHERE SEASON_ID = 22018 AND 
+        (TEAM_ABBREVIATION_HOME = 'DEN' OR TEAM_ABBREVIATION_AWAY = 'DEN');
+--problem: sums all home points and away points (including for opposing teams)
+
+SELECT TEAM_ABBREVIATION_HOME home_team, TEAM_ABBREVIATION_AWAY away_team, PTS_HOME, PTS_AWAY,
+    CASE WHEN TEAM_ABBREVIATION_HOME = 'DEN' THEN PTS_HOME ELSE PTS_HOME = NULL END den_home_pts,
+    CASE WHEN TEAM_ABBREVIATION_AWAY = 'DEN' THEN PTS_AWAY ELSE PTS_AWAY = NULL END den_away_pts
+    FROM newer_games
+    WHERE SEASON_ID = 22018 AND (TEAM_CITY_HOME = 'Denver' OR TEAM_CITY_AWAY = 'Denver');
+--isolates denver's points in each of its 82 games during the 2018-19 season
+
+SELECT 
+    SUM(CASE WHEN TEAM_ABBREVIATION_HOME = 'DEN' THEN PTS_HOME ELSE PTS_HOME = NULL END) den_total_home_pts,
+    SUM(CASE WHEN TEAM_ABBREVIATION_AWAY = 'DEN' THEN PTS_AWAY ELSE PTS_AWAY = NULL END) den_total_away_pts
+    FROM 
+        newer_games
+    WHERE SEASON_ID = 22018 AND (TEAM_CITY_HOME = 'Denver' OR TEAM_CITY_AWAY = 'Denver');
+--returns answer looking for: Denver's total home and away points during 2018-19 season
+
+
+--Question 2:
+--How many total points and points per game did the nuggets score in the 2018-2019 season?    
+SELECT 
+    SUM(CASE WHEN TEAM_ABBREVIATION_HOME = 'DEN' THEN PTS_HOME ELSE PTS_HOME = NULL END) +
+        SUM(CASE WHEN TEAM_ABBREVIATION_AWAY = 'DEN' THEN PTS_AWAY ELSE PTS_AWAY = NULL END) AS den_total_pts,
+    (SUM(CASE WHEN TEAM_ABBREVIATION_HOME = 'DEN' THEN PTS_HOME ELSE PTS_HOME = NULL END) +
+        SUM(CASE WHEN TEAM_ABBREVIATION_AWAY = 'DEN' THEN PTS_AWAY ELSE PTS_AWAY = NULL END))/82 den_ppg
+    FROM newer_games
+    WHERE SEASON_ID = 22018 AND (TEAM_CITY_HOME = 'Denver' OR TEAM_CITY_AWAY = 'Denver');
+    --returns nuggets' total points and ppg for that season
+    
+
+--alternative (cleaner) approach
+--subselect in FROM clause which shows pts in each game
+SELECT ng1.home_team, ng1.away_team, ng1.den_home_pts, ng1.den_away_pts
+    FROM
+    (
+        SELECT ng2.TEAM_ABBREVIATION_HOME home_team, ng2.TEAM_ABBREVIATION_AWAY away_team,
+            (CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'DEN' THEN ng2.PTS_HOME ELSE ng2.PTS_HOME = NULL END) den_home_pts,
+            (CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN ng2.PTS_AWAY ELSE ng2.PTS_AWAY = NULL END) den_away_pts
+        FROM newer_games ng2
+        WHERE ng2.SEASON_ID = 22018 AND (ng2.TEAM_CITY_HOME = 'Denver' OR ng2.TEAM_CITY_AWAY = 'Denver')
+    ) ng1;
+
+--again, subselect in FROM clause, but this time computing statistics requested (total pts and ppg)
+SELECT den_total_pts, den_total_pts/82 den_ppg
+    FROM
+    (
+        SELECT 
+            SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'DEN' THEN ng2.PTS_HOME ELSE ng2.PTS_HOME = NULL END) + 
+            SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN ng2.PTS_AWAY ELSE ng2.PTS_AWAY = NULL END) den_total_pts
+        FROM newer_games ng2
+        WHERE ng2.SEASON_ID = 22018 AND (ng2.TEAM_CITY_HOME = 'Denver' OR ng2.TEAM_CITY_AWAY = 'Denver')
+    ) ng1;
+
+
+--Question 3:
+--How many points total and per game did the nuggets score in the 2018-2019 season broken down by month?
+SELECT 
+    STRFTIME('%Y-%m',ng1.GAME_DATE) month,
+    COUNT(den_home_pts) + COUNT(den_away_pts) games,
+    SUM(ng1.den_home_pts) + SUM(ng1.den_away_pts) den_tot_pts,
+    (SUM(ng1.den_home_pts) + SUM(ng1.den_away_pts))/(COUNT(den_home_pts) + COUNT(den_away_pts)) den_ppg
+    FROM
+    (
+        SELECT ng2.GAME_DATE, ng2.TEAM_ABBREVIATION_HOME home_team, ng2.TEAM_ABBREVIATION_AWAY away_team,
+            (CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'DEN' THEN ng2.PTS_HOME ELSE ng2.PTS_HOME = NULL END) den_home_pts,
+            (CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN ng2.PTS_AWAY ELSE ng2.PTS_AWAY = NULL END) den_away_pts
+        FROM newer_games ng2
+        WHERE ng2.SEASON_ID = 22018 AND (ng2.TEAM_CITY_HOME = 'Denver' OR ng2.TEAM_CITY_AWAY = 'Denver')
+    ) ng1
+    GROUP BY month;
+
+
+
+--Question 4:
+--How many total points and points per game did the nuggets score in each season since 2015-2016?
+--('15-16, '16-17, '17-18, '18-19, '19-20, '20-21)
+SELECT ng1.SEASON_ID, den_total_pts, den_total_pts/82 den_ppg
+    FROM
+    (
+        SELECT 
+            ng2.SEASON_ID,
+            SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'DEN' THEN ng2.PTS_HOME ELSE ng2.PTS_HOME = NULL END) + 
+            SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN ng2.PTS_AWAY ELSE ng2.PTS_AWAY = NULL END) den_total_pts
+        FROM newer_games ng2
+        WHERE ng2.TEAM_CITY_HOME = 'Denver' OR ng2.TEAM_CITY_AWAY = 'Denver'
+        GROUP BY ng2.SEASON_ID
+    ) ng1;
+
+
+--Question 5:
+--Who officiated the 3 longest games in 2016-17?
+SELECT 
+    n.GAME_DATE date, 
+    n.TEAM_ABBREVIATION_HOME home, 
+    n.TEAM_ABBREVIATION_AWAY road, 
+    n.GAME_TIME game_time, 
+    g.FIRST_NAME || ' ' || g.LAST_NAME official --concatenates first & last names of officials
+    FROM newer_games n
+    JOIN Game_Officials g ON n.GAME_ID = g.GAME_ID --join on game ids
+    WHERE SEASON_ID='22019'
+    ORDER BY GAME_TIME DESC --order from longest to shortest
+    LIMIT 9; --return top 9 results as there are 3 refs per game
+--returns the date of the game, matchup, game time (in descending order), and officials' full names
+
+
+--Question 6:
+--How many all-star appearances did the top 5 draft picks of the 1980s average per draft class?
+SELECT d.yearDraft year, d.numberPickOverall draft_pos, d.namePlayer name, p.ALL_STAR_APPEARANCES asg_appear
+    FROM Draft d
+    JOIN Player_Attributes p ON d.idPlayer = p.ID
+    WHERE 
+        numberPickOverall BETWEEN 1 AND 5 AND 
+        yearDraft BETWEEN 1980 AND 1989
+    ORDER BY yearDraft ASC, numberPickOverall ASC;
+--returns the draft year, pick #, player name, and all-star appearances
+--note that there are 49, not 50, results becausee Len Bias died before his rookie year
+
+--now to compute the average all-star game appearances
+SELECT d.yearDraft year, AVG(p.ALL_STAR_APPEARANCES) avg_asg_appear
+    FROM Draft d
+    JOIN Player_Attributes p ON d.idPlayer = p.ID
+    WHERE 
+        d.numberPickOverall BETWEEN 1 AND 5 AND 
+        year BETWEEN 1980 AND 1989
+    GROUP BY year
+    ORDER BY avg_asg_appear DESC;
+--the top 5 picks from 1984 had the most all-star game appearances, on average
+
+
+--Question 7:
+--What are the 3 pt field goal percentages for all teams in the northwest division (jazz, nuggets, 
+--blazers, t'wolves, thunder) during the 2019-2020 season, and how many 3pt fg did they make and attempt?
+SELECT 
+    ng1.den_3pt_made, ng1.den_3pt_attempt, ROUND(ng1.den_3pt_made/ng1.den_3pt_attempt, 3) AS den_3pt_per,
+    ng1.uta_3pt_made, ng1.uta_3pt_attempt, ROUND(ng1.uta_3pt_made/ng1.uta_3pt_attempt, 3) AS uta_3pt_per,
+    ng1.por_3pt_made, ng1.por_3pt_attempt, ROUND(ng1.por_3pt_made/ng1.por_3pt_attempt, 3) AS por_3pt_per,
+    ng1.min_3pt_made, ng1.min_3pt_attempt, ROUND(ng1.min_3pt_made/ng1.min_3pt_attempt, 3) AS min_3pt_per,
+    ng1.okc_3pt_made, ng1.okc_3pt_attempt, ROUND(ng1.okc_3pt_made/ng1.okc_3pt_attempt, 3) AS okc_3pt_per
+    FROM
+    (
+        SELECT 
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'DEN' THEN ng2.FG3M_HOME ELSE ng2.FG3M_HOME = NULL END) + 
+                SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN ng2.FG3M_AWAY ELSE ng2.FG3M_AWAY = NULL END) AS float) den_3pt_made,
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'DEN' THEN ng2.FG3A_HOME ELSE ng2.FG3A_HOME = NULL END) + 
+                SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN ng2.FG3A_AWAY ELSE ng2.FG3A_AWAY = NULL END) AS float) den_3pt_attempt,
+                
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'UTA' THEN ng2.FG3M_HOME ELSE ng2.FG3M_HOME = NULL END) + 
+                SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'UTA' THEN ng2.FG3M_AWAY ELSE ng2.FG3M_AWAY = NULL END) AS float) uta_3pt_made,
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'UTA' THEN ng2.FG3A_HOME ELSE ng2.FG3A_HOME = NULL END) + 
+                SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'UTA' THEN ng2.FG3A_AWAY ELSE ng2.FG3A_AWAY = NULL END) AS float) uta_3pt_attempt,
+            
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'POR' THEN ng2.FG3M_HOME ELSE ng2.FG3M_HOME = NULL END) + 
+                SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'POR' THEN ng2.FG3M_AWAY ELSE ng2.FG3M_AWAY = NULL END) AS float) por_3pt_made,
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'POR' THEN ng2.FG3A_HOME ELSE ng2.FG3A_HOME = NULL END) + 
+                SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'POR' THEN ng2.FG3A_AWAY ELSE ng2.FG3A_AWAY = NULL END) AS float) por_3pt_attempt,
+            
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'MIN' THEN ng2.FG3M_HOME ELSE ng2.FG3M_HOME = NULL END) + 
+                 SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'MIN' THEN ng2.FG3M_AWAY ELSE ng2.FG3M_AWAY = NULL END) AS float) min_3pt_made,
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'MIN' THEN ng2.FG3A_HOME ELSE ng2.FG3A_HOME = NULL END) + 
+                 SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'MIN' THEN ng2.FG3A_AWAY ELSE ng2.FG3A_AWAY = NULL END) AS float) min_3pt_attempt,
+             
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'OKC' THEN ng2.FG3M_HOME ELSE ng2.FG3M_HOME = NULL END) + 
+                 SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'OKC' THEN ng2.FG3M_AWAY ELSE ng2.FG3M_AWAY = NULL END) AS float) okc_3pt_made,
+            CAST(SUM(CASE WHEN ng2.TEAM_ABBREVIATION_HOME = 'OKC' THEN ng2.FG3A_HOME ELSE ng2.FG3A_HOME = NULL END) + 
+                 SUM(CASE WHEN ng2.TEAM_ABBREVIATION_AWAY = 'OKC' THEN ng2.FG3A_AWAY ELSE ng2.FG3A_AWAY = NULL END) AS float) okc_3pt_attempt
+                 
+        FROM newer_games ng2
+        WHERE ng2.SEASON_ID = 22019
+    ) ng1;
---
+++ 02_nba_queries.sql
@@ -0,0 +1,234 @@
+--Questions 8 and 9: 
+--Place Denver's pts for and against during 2015-6 season in two columns
+--Compute quartiles for each stat
+
+--create view
+--contains all of Denver's games from the 2015-16 season and separates out their points and the opposition's points (as well as
+--game id, team ids, attendance, game_time, date, and home and away teams abbrv and cities
+CREATE VIEW den_2015 AS
+SELECT
+    g1.GAME_ID, g1.TEAM_ID_HOME, g1.TEAM_ID_AWAY, g1.attendance, g1.game_time, g1.date,
+    g1.home_team, g1.home_city, g1.home_pts,
+    g1.away_team, g1.away_city, g1.away_pts,
+    g1.team, g1.den_pts, 
+    g1.opp_team, g1.opp_pts
+    FROM (
+        SELECT 
+            g2.GAME_ID, g2.GAME_DATE date, g2.ATTENDANCE attendance, g2.GAME_TIME game_time, g2.TEAM_ID_HOME, g2.TEAM_ID_AWAY,
+            g2.TEAM_ABBREVIATION_HOME home_team, g2.TEAM_ABBREVIATION_AWAY away_team, g2.TEAM_CITY_HOME home_city,
+            g2.TEAM_CITY_AWAY away_city, g2.PTS_HOME home_pts, g2.PTS_AWAY away_pts,
+            'DEN' AS team,
+            TRIM(CASE WHEN g2.TEAM_ABBREVIATION_HOME != 'DEN' THEN g2.TEAM_ABBREVIATION_HOME ELSE g2.TEAM_ABBREVIATION_HOME = '' END ||
+            CASE WHEN g2.TEAM_ABBREVIATION_AWAY != 'DEN' THEN g2.TEAM_ABBREVIATION_AWAY ELSE g2.TEAM_ABBREVIATION_AWAY = ''  END, 0) opp_team,
+            CASE WHEN g2.TEAM_ABBREVIATION_HOME = 'DEN' THEN g2.PTS_HOME ELSE g2.PTS_HOME = 0 END +
+            CASE WHEN g2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN g2.PTS_AWAY ELSE g2.PTS_AWAY = 0 END den_pts,
+            CASE WHEN g2.TEAM_ABBREVIATION_HOME = 'DEN' THEN g2.PTS_AWAY ELSE g2.PTS_AWAY = 0 END +
+            CASE WHEN g2.TEAM_ABBREVIATION_AWAY = 'DEN' THEN g2.PTS_HOME ELSE g2.PTS_HOME = 0 END opp_pts
+            FROM Game g2
+            WHERE g2.SEASON_ID = 22015 
+                AND (g2.TEAM_CITY_HOME = 'Denver' OR  g2.TEAM_CITY_AWAY = 'Denver')
+            ) g1;
+
+--compute quartiles for each stat
+SELECT date, team, den_pts, 
+    NTILE(4) OVER(ORDER BY den_pts) AS quartile,
+    opp_team, opp_pts
+    FROM den_2015
+    ORDER BY date;
+--placing denver's pts into quartiles (1=0-25%, 2=25-50%, etc.) but keeping the game date order
+
+SELECT date, team, den_pts, 
+    NTILE(4) OVER(ORDER BY den_pts) AS den_quartile,
+    opp_team, opp_pts,
+    NTILE(4) OVER(ORDER BY opp_pts) AS opp_quartile
+    FROM den_2015
+    ORDER BY date;
+--similar to above, except now I added the quartile for the opposition's points scored
+
+
+--Question 10: Determine the ranks of the point differential between the Nuggets and their opponents 
+--(from largest win to largest loss) during the 2015-2016 season and display 1) in rank order and 
+--2) in chronological order. 
+--in rank order
+SELECT d1.date, d1.team, d1.den_pts, d1.opp_team, d1.opp_pts, d1.margin,
+        RANK() OVER(ORDER BY margin DESC) AS den_margin_rank
+    FROM(
+        SELECT d2.date, d2.team, d2.den_pts, d2.opp_team, d2.opp_pts, d2.den_pts - d2.opp_pts AS margin
+        FROM den_2015 d2
+        ) d1;
+
+--in chronological order
+SELECT d1.date, d1.team, d1.den_pts, d1.opp_team, d1.opp_pts, d1.margin,
+        RANK() OVER(ORDER BY margin DESC) AS den_margin_rank
+    FROM(
+        SELECT d2.date, d2.team, d2.den_pts, d2.opp_team, d2.opp_pts, d2.den_pts - d2.opp_pts AS margin
+        FROM den_2015 d2
+        ) d1
+    ORDER BY date;
+
+
+--Question 11:
+--Provide the average points that denver and its opponents scored against each other during the 2015-6 season
+--averages as aggregates
+SELECT team, 
+    ROUND(AVG(den_pts),2) avg_den_pts, opp_team, ROUND(AVG(opp_pts),2) avg_opp_pts
+    FROM den_2015
+    GROUP BY opp_team;
+
+--averages as window functions
+SELECT date, team, den_pts,
+        CAST(AVG(den_pts) OVER (PARTITION BY opp_team) AS DECIMAL(5,2)) avg_den_pts,
+        opp_team, opp_pts,
+        CAST(AVG(opp_pts) OVER (PARTITION BY opp_team) AS DECIMAL(5,2)) avg_opp_pts
+        FROM den_2015
+        ORDER BY date;
+
+
+--Question 12:
+--Provide the running average and cumulative points for and against the Nuggets during the 2015-6 season
+SELECT date, team, den_pts,
+        SUM(den_pts) OVER (ROWS UNBOUNDED PRECEDING) cumulative_den_pts,
+        AVG(den_pts) OVER (ROWS UNBOUNDED PRECEDING) running_avg_den_pts,
+        opp_team, opp_pts,
+        SUM(opp_pts) OVER (ROWS UNBOUNDED PRECEDING) cumulative_opp_pts,
+        AVG(opp_pts) OVER (ROWS UNBOUNDED PRECEDING) running_avg_opp_pts
+        FROM den_2015
+        ORDER BY date;
+--works, but decimals are way too long; let's try doing this as a subquery (to be able to use ROUND()
+--in the outer SELECT)
+SELECT d1.date, d1.team, 
+        d1.den_pts, d1.cumulative_den_pts, ROUND(d1.run_avg_den_pts, 2) run_avg_den_pts,
+        d1.opp_team, d1.opp_pts, d1.cumulative_opp_pts, ROUND(d1.run_avg_opp_pts, 2) run_avg_opp_pts
+    FROM (
+        SELECT d2.date, d2.team, d2.den_pts,
+            SUM(d2.den_pts) OVER (ROWS UNBOUNDED PRECEDING) cumulative_den_pts,
+            AVG(d2.den_pts) OVER (ROWS UNBOUNDED PRECEDING) run_avg_den_pts,
+            d2.opp_team, d2.opp_pts,
+            SUM(d2.opp_pts) OVER (ROWS UNBOUNDED PRECEDING) cumulative_opp_pts,
+            AVG(d2.opp_pts) OVER (ROWS UNBOUNDED PRECEDING) run_avg_opp_pts
+            FROM den_2015 d2
+            ORDER BY d2.date
+        ) d1;
+
+
+--Question 13:
+--Question 12, except no running total and break avg down by opponent
+SELECT date, team, den_pts,
+        AVG(den_pts) OVER (PARTITION BY opp_team ROWS UNBOUNDED PRECEDING) run_avg_den_pts,
+        opp_team, opp_pts,
+        AVG(opp_pts) OVER (PARTITION BY opp_team ROWS UNBOUNDED PRECEDING) run_avg_opp_pts
+        FROM den_2015
+        ORDER BY date;
+--ordered by date
+
+SELECT date, team, den_pts,
+        AVG(den_pts) OVER (PARTITION BY opp_team ROWS UNBOUNDED PRECEDING) run_avg_den_pts,
+        opp_team, opp_pts,
+        AVG(opp_pts) OVER (PARTITION BY opp_team ROWS UNBOUNDED PRECEDING) run_avg_opp_pts
+        FROM den_2015
+        ORDER BY opp_team;
+--ordered by opposing team
+        
+
+--Question 14:
+--Return Denver's and its opponents' ppg for each month of the 2015-16 season as window function.
+SELECT d1.month, d1.date, 
+    d1.team, d1.den_pts, ROUND(d1.avg_den_pts, 2) avg_den_pts_month, 
+    d1.opp_team, d1.opp_pts, ROUND(d1.avg_opp_pts, 2) avg_den_pts_month
+    FROM (
+        SELECT STRFTIME('%Y-%m',d2.date) month, d2.date, d2.team, 
+            d2.den_pts,
+            AVG(d2.den_pts) OVER (PARTITION BY STRFTIME('%Y-%m',d2.date)) avg_den_pts,
+            d2.opp_team, d2.opp_pts,
+            AVG(d2.opp_pts) OVER (PARTITION BY STRFTIME('%Y-%m',d2.date)) avg_opp_pts
+            FROM den_2015 d2
+            ) d1
+    ORDER BY month;
+--using FROM subquery
+    
+SELECT STRFTIME('%Y-%m',d1.date) month, d1.date, d1.team, 
+            d1.den_pts,
+            (SELECT ROUND(AVG(d2.den_pts),2) 
+                FROM den_2015 d2
+                WHERE STRFTIME('%Y-%m',d2.date) = STRFTIME('%Y-%m',d1.date)) avg_den_pts_month,
+            d1.opp_team, d1.opp_pts,
+            (SELECT ROUND(AVG(d3.opp_pts),2)
+                FROM den_2015 d3 
+                WHERE STRFTIME('%Y-%m',d3.date) = STRFTIME('%Y-%m',d1.date)) avg_opp_pts_month
+            FROM den_2015 d1
+    ORDER BY month;
+--using SELECT subquery
+
+
+--Question 15:
+--What's the avg margin of victory/defeat for each month of the 2015-16 season?
+SELECT STRFTIME('%Y-%m',d1.date) month, d1.date, d1.team, 
+            d1.den_pts, d1.opp_team, d1.opp_pts, d1.den_pts-d1.opp_pts margin,
+            (SELECT ROUND(AVG(d2.den_pts) - AVG(d2.opp_pts), 2)
+                FROM den_2015 d2
+                WHERE STRFTIME('%Y-%m',d2.date) = STRFTIME('%Y-%m',d1.date)) avg_margin_month
+            FROM den_2015 d1
+    ORDER BY month;
+
+
+--Question 16:
+--What were the 3 largest victories and 3 largest defeats (including ties) for the Nuggets in home and away games during
+--the 2015-16 season? Place in the following order: away games--largest victories to largest defeats--then home games--same
+
+--calculate margins for home and away games, sort for largest victories/defeats, and union them
+SELECT d1.*
+    FROM
+    (SELECT date, home_team, den_pts, away_team, opp_pts,
+        den_pts - opp_pts den_margin
+        FROM den_2015 
+        ORDER BY den_margin DESC
+        LIMIT 3) d1
+UNION 
+SELECT d2.*
+    FROM
+    (SELECT date, home_team, den_pts, away_team, opp_pts,
+        den_pts - opp_pts den_margin
+        FROM den_2015 
+        ORDER BY den_margin ASC
+        LIMIT 3) d2
+    ORDER BY den_margin DESC;
+--this is for all games
+
+--set up CASE statement to separate when Denver is home/away and sort by this factor
+SELECT d1.*
+    FROM
+        (SELECT date, home_team, away_team, den_pts, opp_pts, den_pts - opp_pts den_margin,
+        CASE WHEN home_team = 'DEN' THEN 1 ELSE 0 END home_away
+        FROM den_2015 
+        WHERE home_away= 0
+        ORDER BY den_margin DESC
+        LIMIT 3) d1
+UNION
+SELECT d2.*
+    FROM
+        (SELECT date, home_team, away_team, den_pts, opp_pts, den_pts - opp_pts den_margin,
+        CASE WHEN home_team = 'DEN' THEN 1 ELSE 0 END home_away
+        FROM den_2015 
+        WHERE home_away = 0
+        ORDER BY den_margin ASC
+        LIMIT 3) d2
+UNION
+SELECT d3.*
+    FROM
+        (SELECT date, home_team, away_team, den_pts, opp_pts, den_pts - opp_pts den_margin,
+        CASE WHEN home_team = 'DEN' THEN 1 ELSE 0 END home_away
+        FROM den_2015 
+        WHERE home_away = 1
+        ORDER BY den_margin DESC
+        LIMIT 4) d3
+UNION
+SELECT d4.*
+    FROM
+        (SELECT date, home_team, away_team, den_pts, opp_pts, den_pts - opp_pts den_margin,
+        CASE WHEN home_team = 'DEN' THEN 1 ELSE 0 END home_away
+        FROM den_2015 
+        WHERE home_away = 1
+        ORDER BY den_margin ASC
+        LIMIT 4) d4
+    ORDER BY home_away, den_margin DESC;
+--returns 3 largest victories and defeats, first on the road then at home, including ties
